280b376d91907d67ac3ea94601be8dde5c5fdb43
NAMNGUYEN\NAM NGUYEN <nam.nguyen1308hcmut@hcmut.edu.vn>
2024-09-19 23:09:11 +0700

    no message

---
+++ EX/LAB1_EX/Core/Inc/ex7_to_ex10.h
@@ -0,0 +1,15 @@
+/*
+ * ex7_to_ex10.h
+ *
+ *  Created on: Sep 19, 2024
+ *      Author: NAM NGUYEN
+ */
+
+#ifndef INC_EX7_TO_EX10_H_
+#define INC_EX7_TO_EX10_H_
+
+void ex7_to_ex10();
+void clearAllClock();
+void setNumberOnClock(int num);
+void clearNumberOnClock(int num);
+#endif /* INC_EX7_TO_EX10_H_ */
